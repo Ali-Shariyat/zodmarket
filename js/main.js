92415484dc6637cc953d9567383dfcc940a9012b
Ali-Shariyat <ali.programmer78@gmail.com>
2020-01-02 17:25:31 +0330

    fix footer ,fix more bugs.

--- js/main.js
+++ js/main.js
@@ -19,21 +19,87 @@ $(".menu-items ul li div i.icon-font-plus-menu").click(function () {
     $(this).toggleClass("icon-font-remove-menu");
 });
 
-$('.slider-home-page').each(function () {
+$('[data-slider]').each(function () {
     var $this = $(this);
-    $this.find(".slider").flickity({
+    var option = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: eval($this.attr("data-disable-number")),
+        wrapAround: eval($this.attr("data-loop")),
+        setGallerySize: false,
+        resize: true,
         prevNextButtons: false,
-        rightToLeft: true,
-        pageDots: false,
-        wrapAround: true,
-        autoPlay: 1500
-    });
+        cellSelector: ".item",
+        pageDots: eval($this.attr("data-items-nav")),
+        autoPlay: eval($this.attr("data-autoPlay")),
+        pauseAutoPlayOnHover: eval($this.attr("data-pauseAutoPlayOnHover")),
+        rightToLeft: true
+    };
+    $this.find(".this-slider").flickity(option);
+    setTimeout(function () {
+        $this.height($this.find(".item").outerHeight(true));
+        $this.find(".this-slider").height($this.find(".item").outerHeight(true));
+    }, 10);
     $this.find(".chevron-left").click(function () {
-        $this.find(".slider").flickity('next');
-    })
+        $this.find(".this-slider").flickity('next');
+    });
     $this.find(".chevron-right").click(function () {
-        $this.find(".slider").flickity('previous');
-    })
+        $this.find(".this-slider").flickity('previous');
+    });
+    $(window).on('ready load resize orientationchange', function () {
+        $this.height($this.find(".item").outerHeight(true));
+        $this.find(".this-slider").height($this.find(".item").outerHeight(true));
+
+        if ($(this).width() <= 600) {
+            $this.attr("data-disable-number", $this.attr("data-disable-number-s"));
+            if ($this.find(".this-slider .item").length <= $this.attr("data-disable-number")) {
+                option["draggable"] = false;
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+            } else {
+                option["draggable"] = true;
+                option["groupCells"] = eval($this.attr("data-disable-number"));
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+
+                $this.find(".this-slider").flickity('selectCell', 1);
+
+
+            }
+        }
+        if ($(this).width() > 601 && $(this).width() < 991) {
+            $this.attr("data-disable-number", $this.attr("data-disable-number-m"));
+            if ($this.find(".this-slider .item").length <= $this.attr("data-disable-number")) {
+                option["draggable"] = false;
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+            } else {
+                option["draggable"] = true;
+                option["groupCells"] = eval($this.attr("data-disable-number"));
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+            }
+        }
+        if ($(this).width() >= 992) {
+            $this.attr("data-disable-number", $this.attr("data-disable-number-l"));
+            if ($this.find(".this-slider .item").length <= $this.attr("data-disable-number")) {
+                option["draggable"] = false;
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+            } else {
+                option["draggable"] = true;
+                option["groupCells"] = eval($this.attr("data-disable-number"));
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+            }
+        }
+        if ($(this).width() > 1200) {
+            $this.attr("data-disable-number", $this.attr("data-disable-number-xl"));
+            if ($this.find(".this-slider .item").length <= $this.attr("data-disable-number")) {
+                option["draggable"] = false;
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+            } else {
+                option["draggable"] = true;
+                option["groupCells"] = eval($this.attr("data-disable-number"));
+                $this.find(".this-slider").flickity('destroy').flickity(option);
+            }
+
+        }
+    });
 });
 //==================
 // niceScroll touch
