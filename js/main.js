ff092123169cd34079c84ffe8fbeba3dfdf0fd8a
Ali-Shariyat <ali.programmer78@gmail.com>
2020-01-02 09:59:00 +0330

    add modal box search ,fix responsive header ,fix responsive menu ,change scroll box ,fix more bugs.

--- js/main.js
+++ js/main.js
@@ -13,8 +13,38 @@ $(".black-page , .close-menu i").click(function () {
     $("html").removeAttr("style");
 });
 
-$(".menu-items ul li div i.fas.fa-plus").click(function () {
+$(".menu-items ul li div i.icon-font-plus-menu").click(function () {
     $(this).parents("ul li").find("ul.menu-down").stop().slideToggle();
-    $(this).toggleClass("fa-plus");
-    $(this).toggleClass("fa-minus");
+    $(this).toggleClass("icon-font-plus-menu");
+    $(this).toggleClass("icon-font-remove-menu");
+});
+
+$('.slider-home-page').each(function () {
+    var $this = $(this);
+    $this.find(".slider").flickity({
+        prevNextButtons: false,
+        rightToLeft: true,
+        pageDots: false,
+        wrapAround: true,
+        autoPlay: 1500
+    });
+    $this.find(".chevron-left").click(function () {
+        $this.find(".slider").flickity('next');
+    })
+    $this.find(".chevron-right").click(function () {
+        $this.find(".slider").flickity('previous');
+    })
+});
+//==================
+// niceScroll touch
+//==================
+
+$(window).on('ready load resize change', function () {
+    if ($("[data-scroll]").length) {
+        $("[data-scroll]").niceScroll({
+            cursorwidth: "7px",
+            touchbehavior: true,
+            rtlmode: "auto"
+        });
+    }
 });
